9b413de372872b2d398a68ee3d38f94cf7dc0170
Maiko Karahashi <yamyam412000@gmail.com>
2025-04-20 20:51:13 +0900

    5章途中まで

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,48 +1,10 @@
 import Image from "next/image";
+import { getMemberList } from "../_libs/microcms";
 import styles from "./page.module.css";
+import { MEMBERS_LIST_LIMIT } from "../_constants";
 
-const data = {
-  contents: [
-    {
-      id: "1",
-      image: {
-        url: "/img-member1.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "デイビッド・チャン",
-      position: "CEO",
-      profile:
-        "グローバルテクノロジー企業での豊富な経験を持つリーダー。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功。自身の経験と洞察力により、業界のトレンドを見極めて戦略的な方針を策定し、会社の成長をけん引している。",
-    },
-    {
-      id: "2",
-      image: {
-        url: "/img-member2.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "エミリー・サンダース",
-      position: "COO",
-      profile:
-        "グローバル企業での運営管理と組織改革の経験豊富なエグゼクティブ。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功。自身の経験と洞察力により、業界のトレンドを見極めて戦略的な方針を策定し、会社の成長をけん引している。",
-    },
-    {
-      id: "3",
-      image: {
-        url: "/img-member3.jpg",
-        width: 240,
-        height: 240,
-      },
-      name: "ジョン・ウィルソン",
-      position: "CTO",
-      profile:
-        "先進技術の研究開発と製品イノベーションの分野で優れた経歴を持つテクノロジーエキスパート。以前は大手ソフトウェア企業の上級幹部として勤務し、新市場進出や収益成長に成功。自身の経験と洞察力により、業界のトレンドを見極めて戦略的な方針を策定し、会社の成長をけん引している。",
-    },
-  ],
-};
-
-export default function Page() {
+export default async function Page() {
+  const data = await getMemberList({ limit: MEMBERS_LIST_LIMIT });
   return (
     <div className={styles.container}>
       {data.contents.length === 0 ? (
