dcc807a68ac757f3f5875a3ec185d90ae3f8f44c
Maiko Karahashi <yamyam412000@gmail.com>
2025-05-05 20:00:48 +0900

    7章まで完了

--- src/app/_libs/microcms.ts
+++ src/app/_libs/microcms.ts
@@ -4,6 +4,7 @@ import type {
   MicroCMSImage,
   MicroCMSListContent,
 } from "microcms-js-sdk";
+import { StringifyOptions } from "querystring";
 
 export type Member = {
   name: string;
@@ -14,17 +15,15 @@ export type Member = {
 
 export type Category = {
   name: string;
-};
+} & MicroCMSListContent;
 
 export type News = {
-  id: string;
   title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+  description: string;
+  content: string;
+  thumbnail?: MicroCMSImage;
+  category: Category;
+} & MicroCMSListContent;
 
 if (!process.env.MICROCMS_SERVICE_DOMAIN) {
   throw new Error("MICROCMS_SERVICE_DOMAIN is required");
@@ -45,4 +44,36 @@ export const getMemberList = async (queries?: MicroCMSQueries) => {
     queries,
   });
   return listData;
+};
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<News>({
+    endpoint: "news",
+    queries,
+  });
+  return listData;
+};
+
+export const getNewsDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  const detailData = await client.getListDetail<News>({
+    endpoint: "news",
+    contentId,
+    queries,
+  });
+  return detailData;
+};
+
+export const getCategoryDetail = async (
+  contentId: string,
+  queries: MicroCMSQueries
+) => {
+  const detailData = await client.getListDetail<Category>({
+    endpoint: "categories",
+    contentId,
+    queries,
+  });
+  return detailData;
 };
