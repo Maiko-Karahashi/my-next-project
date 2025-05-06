72e4901cd9e35d82d9a6727e92f9de5f89e9e0bf
Maiko Karahashi <yamyam412000@gmail.com>
2025-05-06 15:01:51 +0900

    修正

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,16 +1,11 @@
-import { notFound } from "next/navigation";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
 type Props = {
-  params: {
-    slug: string;
-  };
-  searchParams: {
-    dk?: string;
-  };
+  params: { slug: string };
+  searchParams: { dk?: string };
 };
 
 export default async function Page({ params, searchParams }: Props) {
