a599a21cda83bd6945dfd3c27d719f0df7d13e69
Maiko Karahashi <yamyam412000@gmail.com>
2025-05-06 15:09:21 +0900

    修正

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -4,13 +4,20 @@ import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
 type Props = {
-  params: { slug: string };
-  searchParams: { dk?: string };
+  params: {
+    slug: string;
+  };
+  searchParams?: {
+    dk?: string;
+  };
 };
 
-export default async function Page({ params, searchParams }: Props) {
+export default async function Page({ params, searchParams }: {
+  params: { slug: string };
+  searchParams?: { dk?: string };
+}) {
   const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams.dk,
+    draftKey: searchParams?.dk,
   });
 
   return (
